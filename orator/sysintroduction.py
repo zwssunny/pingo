223b959f80daee3b720c9bb637180153b77a2131
zhanws <zwssunny@qq.com>
2023-10-10 15:12:57 +0800

    更改页面序号

--- orator/sysintroduction.py
+++ orator/sysintroduction.py
@@ -80,4 +80,4 @@ if __name__ == '__main__':
     tts = EdgeVoice()
     sysIntro = sysIntroduction(tts, True)
     # sysIntro.menutalk()
-    sysIntro.talkitem_byname("首页")
+    sysIntro.talkitem_byname("城市交通.道路运行")
