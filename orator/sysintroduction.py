957dc327c6d43a4cb06fddeca1ef74f56d88b1f4
zhanws <zwssunny@qq.com>
2023-10-21 18:46:42 +0800

    增加多剧本编排功能

--- orator/sysintroduction.py
+++ orator/sysintroduction.py
@@ -25,6 +25,53 @@ class sysIntroduction:
         self.askcontinu = askcontinu
         self.pagecontrol = pagecontrol()
 
+    def billtalk(self):
+        """
+        演示整个系统
+        """
+        try:
+            # 查询系统介绍内容
+            cursor = self.conn.execute(
+                "SELECT ID, NAME,VOICE, DESC FROM BILL WHERE ISDEFAULT=1")
+            billcursor = cursor.fetchone()
+            if billcursor:
+                billdesc = billcursor[3]
+                billid=billcursor[0]
+                self.tts.text_to_speech_and_play(billdesc)
+                self.talkAllBillItem(billid)
+            else:
+                self.tts.text_to_speech_and_play("找不到剧本")
+        except sqlite3.Error as error:
+            logger.error(error)
+
+    def talkAllBillItem(self,billID):
+        """
+        解说的该剧本所有内容
+        """
+        try:
+            # 查询菜单记录
+            cursor = self.conn.execute(
+                "SELECT TYPENAME,TYPEID, ORDERNO FROM BILLITEM WHERE ENABLE=1 AND BILLID= ? ORDER BY ORDERNO",(billID,))
+            itemcursor = cursor.fetchall()
+            for row in itemcursor:
+                # if self.askcontinu:
+                #     willcontinue = input("继续演示吗？")
+                #     if willcontinue == "n":
+                #         return
+                    
+                typename=row[0]
+                typeid=row[1]
+                if typename=='MENUITEM':
+                    self.talkitem_byid(typeid)
+                elif typename=='FEATURES':
+                    self.talkfeature_byid(typeid)
+                elif typename=='OTHERSYSTEM':
+                    self.talkothersystem_byid(typeid)
+                elif typename=='HIGHLIGHT':
+                    self.talkhighlight_byid(typeid)
+        except sqlite3.Error as error:
+            logger.error(error)           
+
     def systalk(self):
         """
         演示整个系统
@@ -79,7 +126,7 @@ class sysIntroduction:
                 self.menuitemtalk(menucursor)
         except sqlite3.Error as error:
             logger.error(error)
-            
+
     def talkitem_byid(self, menuid):
         """
         解说某个菜单页面
@@ -94,6 +141,78 @@ class sysIntroduction:
             menucursor = cursor.fetchone()
             if menucursor:
                 self.menuitemtalk(menucursor)
+        except sqlite3.Error as error:
+            logger.error(error)
+
+    def talkothersystem_byid(self, othersystemid):
+        """
+        解说某个第三方系统
+
+        Args:
+            othersystemid (integer): 第三方系统id
+        """
+        try:
+            # 查询菜单记录
+            cursor = self.conn.execute(
+                "SELECT NAME, ORDERNO, PAGEINDEX, DESC FROM OTHERSYSTEM WHERE ID = ?", (othersystemid,))
+            itemcursor = cursor.fetchone()
+            if itemcursor:
+                # 发送页面切换指令
+                pageindex = itemcursor[2]
+                self.pagecontrol.sendPageCtl("OPEN_SYSTEM", pageindex)
+                # 介绍页面功能
+                itemname = itemcursor[0]
+                itemdesc = itemcursor[3]
+                logger.info("讲解" + itemname)
+                self.tts.text_to_speech_and_play(itemname)
+                self.tts.text_to_speech_and_play(itemdesc, self.askcontinu)
+        except sqlite3.Error as error:
+            logger.error(error)
+
+    def talkhighlight_byid(self, highlightid):
+        """
+        解说某个亮点场景
+
+        Args:
+            highlightid (integer): 亮点场景id
+        """
+        try:
+            # 查询菜单记录
+            cursor = self.conn.execute(
+                "SELECT NAME, ORDERNO, PAGEINDEX, DESC FROM HIGHTLIGHT WHERE ID = ?", (highlightid,))
+            highlightcursor = cursor.fetchone()
+            if highlightcursor:
+                # 发送页面切换指令
+                pageindex = highlightcursor[2]
+                self.pagecontrol.sendPageCtl("OPEN_HIGHLIGHT", pageindex)
+                # 介绍页面功能
+                itemname = highlightcursor[0]
+                itemdesc = highlightcursor[3]
+                logger.info("讲解" + itemname)
+                self.tts.text_to_speech_and_play(itemname)
+                self.tts.text_to_speech_and_play(itemdesc, self.askcontinu)
+        except sqlite3.Error as error:
+            logger.error(error)
+
+    def talkfeature_byid(self, featureid):
+        """
+        解说某个第三方系统
+
+        Args:
+            othersystemid (integer): 第三方系统id
+        """
+        try:
+            # 查询菜单记录
+            cursor = self.conn.execute(
+                "SELECT NAME, ORDERNO,  DESC FROM FEATURES WHERE ID = ?", (featureid,))
+            itemcursor = cursor.fetchone()
+            if itemcursor:
+                # 介绍页面功能
+                itemname = itemcursor[0]
+                itemdesc = itemcursor[2]
+                logger.info("讲解" + itemname)
+                self.tts.text_to_speech_and_play(itemname)
+                self.tts.text_to_speech_and_play(itemdesc, self.askcontinu)
         except sqlite3.Error as error:
             logger.error(error)
 
@@ -121,5 +240,6 @@ if __name__ == '__main__':
     from voice.edge.EdgeVoice import EdgeVoice
     tts = EdgeVoice()
     sysIntro = sysIntroduction(tts, False)
-    # sysIntro.menutalk()
-    sysIntro.talkitem_byname("城市交通.地铁")
+    sysIntro.billtalk() #剧本
+    # sysIntro.menutalk() #所有大屏页面
+    # sysIntro.talkitem_byname("城市交通.地铁")
