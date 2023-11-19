d00a6caa3ca8c600ba56f7fe8baa2d2723e24357
zwssunny <zwssunny@qq.com>
2023-11-19 08:54:30 +0800

    完善docker部署

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -5,4 +5,4 @@ unset KUBECONFIG
 cd .. && docker build -f docker/Dockerfile.latest \
              -t zhanws/pingo .
 
-docker tag zhanws/pingo  zhanws/pingo:$(date +%y%m%d)
+# docker tag zhanws/pingo  zhanws/pingo:$(date +%y%m%d)
