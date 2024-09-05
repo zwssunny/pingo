253dfefd043b69120f9d63da55fcd01cd9aaff31
zhanws <zwssunny@qq.com>
2024-09-05 15:29:17 +0800

    升级唤醒词版本号

--- gvar.py
+++ gvar.py
@@ -1,11 +1,11 @@
-
 from config import conf, load_config
+
 
 class GVar(object):
     # 会话,Conversation
-    conversation=None
-    #Pingo
+    conversation = None
+    # Pingo
     pingo = None
     load_config()
-    serverconf = conf().get("server",{"host": "0.0.0.0","port": "5001"})
+    serverconf = conf().get("server", {"host": "0.0.0.0", "port": "5001"})
     sysdb = "./db/pingo.db"
